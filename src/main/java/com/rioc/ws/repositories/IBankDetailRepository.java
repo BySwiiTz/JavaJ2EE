f2eb9a02b88419ad72136856951182e286a7ba60
Mathieu Linkès <mathieu.linkes@hotmail.com>
2022-11-22 18:00:46 +0100

    2eme commit

--- src/main/java/com/rioc/ws/repositories/IBankDetailRepository.java
+++ src/main/java/com/rioc/ws/repositories/IBankDetailRepository.java
@@ -1,6 +1,8 @@
 package com.rioc.ws.repositories;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,5 +11,5 @@ import com.rioc.ws.modeles.dao.BankDetail;
 
 @Repository
 public interface IBankDetailRepository extends JpaRepository<BankDetail, Integer>{
-    public BankDetail findByIban(@Param(value = "iban") String iban);
+    public Optional<BankDetail> findByIban(@Param(value = "iban") String iban);
 }
--- src/main/java/com/rioc/ws/services/bankDetail/BankDetailService.java
+++ src/main/java/com/rioc/ws/services/bankDetail/BankDetailService.java
@@ -2,6 +2,7 @@ package com.rioc.ws.services.bankDetail;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -18,13 +19,13 @@ import nl.garvelink.iban.IBAN;
 import nl.garvelink.iban.Modulo97;
 
 @Service
-public class BankDetailService implements IBankDetailService{
-    
+public class BankDetailService implements IBankDetailService {
+
     private IBankDetailRepository repository;
     private IAccountService accountService;
     private IAccountMapper mapper;
 
-    public BankDetailService(IBankDetailRepository repository, IAccountMapper mapper, IAccountService accountService){
+    public BankDetailService(IBankDetailRepository repository, IAccountMapper mapper, IAccountService accountService) {
         super();
         this.repository = repository;
         this.mapper = mapper;
@@ -35,20 +36,23 @@ public class BankDetailService implements IBankDetailService{
         return Modulo97.verifyCheckDigits(bankDetailDto.getIban());
     }
 
-    public BankDetail postBankDetail(BankDetailDto bankDetailDto){
+    public BankDetail postBankDetail(BankDetailDto bankDetailDto) {
         Account account = accountService.getAccountByPseudo(bankDetailDto.getPseudo());
-        if (bankDetailDto.getIban() != null && bankDetailDto.getIban().chars().filter(c -> c == (int)' ').count() < 1 ){
-                try {
-                    if(repository.findByIban(bankDetailDto.getIban()) == null && validateIBAN(bankDetailDto)){
-                        IBAN iban = IBAN.valueOf(bankDetailDto.getIban());
-                        bankDetailDto.setIban(iban.toString());
+        if (bankDetailDto.getIban() != null
+                && bankDetailDto.getIban().chars().filter(c -> c == (int) ' ').count() < 1) {
+            try {
+                if (validateIBAN(bankDetailDto)) {
+                    IBAN iban = IBAN.valueOf(bankDetailDto.getIban());
+                    bankDetailDto.setIban(iban.toString());
+                    if (repository.findByIban(bankDetailDto.getIban()).isEmpty()) {
                         BankDetail bankDetail = mapper.bankDetailDtoToBankDetail(bankDetailDto);
                         bankDetail.setAccount(account);
                         return repository.save(bankDetail);
                     }
-                } catch (Exception e) {
-                    throw new ApiException("IBAN ERROR", HttpStatus.BAD_REQUEST);
                 }
+            } catch (Exception e) {
+                throw new ApiException("IBAN ERROR", HttpStatus.BAD_REQUEST);
+            }
             throw new ApiException("IBAN FORMAT INCORRECT", HttpStatus.BAD_REQUEST);
         }
         throw new ApiException("IBAN FORMAT INCORRECT (WITHOUT SPACE)", HttpStatus.BAD_REQUEST);
@@ -65,13 +69,13 @@ public class BankDetailService implements IBankDetailService{
         return listeByPseudo;
     }
 
-    public BankDetailDto deleteByIban(String iban){
+    public BankDetailDto deleteByIban(String iban) {
         IBAN ib = IBAN.valueOf(iban);
-        BankDetail delete = repository.findByIban(ib.toString());
-        if (delete == null) {
+        Optional<BankDetail> delete = repository.findByIban(ib.toString());
+        if (delete.isEmpty()) {
             throw new ApiException("IBAN DOESN'T EXIST", HttpStatus.NOT_FOUND);
         }
-        repository.delete(delete);
-        return mapper.bankDetailToBankDetailDto(delete);
+        repository.delete(delete.get());
+        return mapper.bankDetailToBankDetailDto(delete.get());
     }
 }
